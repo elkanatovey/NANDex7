e6a799a77ea4a4b029c95333a87beef7acd96982
Aviad Dudkevich <aviadud@cs.huji.ac.il>
2018-11-17 22:38:33 +0200

    Update VMTranslator.py

--- VMTranslator.py
+++ VMTranslator.py
@@ -34,6 +34,42 @@ def get_files(args):
     """
     list_of_files_path = []
     if len(args) == NUMBER_OF_ARGS:
+##############################################################################
+# FILE : VMTranslatorpy
+# WRITER : Aviad Dudkewitz, Elkana Tovey
+# DESCRIPTION: This program translate VM code to Hack assembly language.
+##############################################################################
+import sys
+import re
+from pathlib import Path
+from os import listdir
+
+NUMBER_OF_ARGS = 2
+
+INVALID_ARGS = "The file given as input is invalid..."
+VALID_INPUT_SUFFIX = ".*\.vm$"
+VM_SUFFIX = "\.vm$"
+ASSEMBLY_SUFFIX = ".asm"
+EMPTY_LINE = "^\s*$"
+COMMENT = "//.*$"
+
+ARITHMETIC_COMMAND = ["add", "sub", "neg", "eq", "gt", "lt", "and", "or",
+                      "not"]
+MEMORY_ACCESS_COMMAND = ["push", "pop"]
+PROGRAM_FLOW_COMMAND = ["label", "goto", "if-goto"]
+FUNCTION_CALLING_COMMAND = ["function", "call", "return"]
+COMPARISON_JUMP_COMMAND = {"eq": "JEQ", "gt": "JGT", "lt": "JLT"}
+
+vm_suffix_pattern = re.compile(VALID_INPUT_SUFFIX)
+
+
+def get_files(args):
+    """
+    :param args: the arguments given to the program.
+    :return: the list of paths to .vm files
+    """
+    list_of_files_path = []
+    if len(args) == NUMBER_OF_ARGS:
         if Path(args[1]).is_file() and vm_suffix_pattern.match(args[1]):
             list_of_files_path.append(args[1])
         elif Path(args[1]).is_dir():
@@ -73,66 +109,102 @@ def file_to_assembly_lines(file_path):
     """
     result = []
     with open(file_path, "r") as input_file:
-        num_of_comparison = 0
+        comparison_counter = 0
         for line in input_file:
             line = re.sub(COMMENT, "", line)
             line_as_list = line.split()
             if len(line_as_list) > 0:
-                print(line_as_list) # for testing.
+                print(line_as_list)  # for testing.
                 if line_as_list[0] in ARITHMETIC_COMMAND:
-                    result += get_arithmetic_command_lines(line_as_list[0])
+                    temp_result, comparison_counter = \
+                        get_arithmetic_command_lines(line_as_list[0],
+                                                     comparison_counter)
+                    result += temp_result
                 elif line_as_list[0] in MEMORY_ACCESS_COMMAND:
                     result += get_memory_command_lines(line_as_list[0],
                                                        line_as_list[1],
-                                                       line_as_list[3])
+                                                       line_as_list[2])
                 elif line_as_list[0] in PROGRAM_FLOW_COMMAND:
-                    pass # for project 8
+                    pass  # for project 8
                 elif line_as_list[0] in FUNCTION_CALLING_COMMAND:
-                    pass # for project 8
+                    pass  # for project 8
     return result
 
 
-def get_arithmetic_command_lines(command):
-        if command == "add":
-            return ["//add:",
-                    "@SP",
-                    "A = M - 1",
-                    "D = M",
-                    "@SP",
-                    "M = M - 1",
-                    "A = M - 1",
-                    "M = M + D"]
-        if command == "sub":
-            return ["//sub:",
-                    "@SP",
-                    "A = M - 1",
-                    "D = M",
-                    "@SP",
-                    "M = M - 1",
-                    "A = M - 1",
-                    "M = M - D"]
-        if command == "neg":
-            return ["//neg:",
-                    "@SP",
-                    "A = M - 1",
-                    "M = -M"]
-        if command == "eq":
-            result = ["//" + command + ":",
-                      "@SP",
-                      "A = M - 1",
-                      "D = M // D=y",
-                      "@SP",
-                      "M = M - 1",
-                      "A = M - 1",
-                      "D = M - D // D=x-y"]
-                      #not finished...
+def get_arithmetic_command_lines(command, comparison_counter):
+    if command == "add":
+        return ["//add:",
+                "@SP",
+                "A = M - 1",
+                "D = M",
+                "@SP",
+                "M = M - 1",
+                "A = M - 1",
+                "M = M + D"], comparison_counter
+    if command == "sub":
+        return ["//sub:",
+                "@SP",
+                "A = M - 1",
+                "D = M",
+                "@SP",
+                "M = M - 1",
+                "A = M - 1",
+                "M = M - D"], comparison_counter
+    if command == "neg":
+        return ["//neg:",
+                "@SP",
+                "A = M - 1",
+                "M = -M"], comparison_counter
+    if command in COMPARISON_JUMP_COMMAND.keys():
+        comparison_counter += 1
+        return ["//" + command + ":",
+                "@SP",
+                "A = M - 1",
+                "D = M // D=y",
+                "@SP",
+                "M = M - 1",
+                "A = M - 1",
+                "D = M - D // D=x-y",
+                "@" + str(comparison_counter) + "COMPARISON",
+                "D;" + COMPARISON_JUMP_COMMAND[command],
+                "M = 0",
+                "@" + str(comparison_counter) + "COMPARISON_END",
+                "0;JMP",
+                "(" + str(comparison_counter) + "COMPARISON)",
+                "M = -1",
+                "(" + str(comparison_counter) + "COMPARISON_END)"], \
+               comparison_counter
+    if command == "and":
+        return ["//and:",
+                "@SP",
+                "A = M - 1",
+                "D = M",
+                "@SP",
+                "M = M - 1",
+                "A = M - 1",
+                "M = D&M"], comparison_counter
+    if command == "or":
+        return ["//or:",
+                "@SP",
+                "A = M - 1",
+                "D = M",
+                "@SP",
+                "M = M - 1",
+                "A = M - 1",
+                "M = D|M"], comparison_counter
+    if command == "not":
+        return ["//not:",
+                "@SP",
+                "A = M - 1",
+                "M = !M"], comparison_counter
 
 
 def get_memory_command_lines(command, segment, index):
     if command == "push":
-        pass
+        return []
     elif command == "pop":
-        pass
+        return []
+
 
 # The main program:
 if __name__ == "__main__":
