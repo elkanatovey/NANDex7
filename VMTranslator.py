4f025c4ecc35cf0f5cb3ac9b4af3a4e323077324
Aviad Dudkevich <aviadud@cs.huji.ac.il>
2018-11-17 19:47:02 +0200

    Update VMTranslator.py

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # FILE : VMTranslatorpy
-# WRITER : Aviad Dudkewitz, Alkana Tovey
+# WRITER : Aviad Dudkewitz, Elkana Tovey
 # DESCRIPTION: This program translate VM code to Hack assembly language.
 ##############################################################################
 import sys
@@ -15,8 +15,14 @@ INVALID_ARGS = "The file given as input is invalid..."
 VALID_INPUT_SUFFIX = ".*\.vm$"
 VM_SUFFIX = "\.vm$"
 ASSEMBLY_SUFFIX = ".asm"
-EMPTY_LINE = "(^\s$)|(^//)"
+EMPTY_LINE = "^\s*$"
 COMMENT = "//.*$"
+
+ARITHMETIC_COMMAND = ["add", "sub", "neg", "eq", "gt", "lt", "and", "or",
+                      "not"]
+MEMORY_ACCESS_COMMAND = ["push", "pop"]
+PROGRAM_FLOW_COMMAND = ["label", "goto", "if-goto"]
+FUNCTION_CALLING_COMMAND = ["function", "call", "return"]
 
 vm_suffix_pattern = re.compile(VALID_INPUT_SUFFIX)
 
@@ -59,9 +65,79 @@ def lines_list_to_file(file_path, lines_list):
             output_file.write(line + "\n")
 
 
+def file_to_assembly_lines(file_path):
+    """
+    Takes a vm file and translate all lines to Hack assembly language.
+    :param file_path:  a string representing a the file path.
+    :return: a list with all the translated lines.
+    """
+    result = []
+    with open(file_path, "r") as input_file:
+        num_of_comparison = 0
+        for line in input_file:
+            line = re.sub(COMMENT, "", line)
+            line_as_list = line.split()
+            if len(line_as_list) > 0:
+                print(line_as_list) # for testing.
+                if line_as_list[0] in ARITHMETIC_COMMAND:
+                    result += get_arithmetic_command_lines(line_as_list[0])
+                elif line_as_list[0] in MEMORY_ACCESS_COMMAND:
+                    result += get_memory_command_lines(line_as_list[0],
+                                                       line_as_list[1],
+                                                       line_as_list[3])
+                elif line_as_list[0] in PROGRAM_FLOW_COMMAND:
+                    pass # for project 8
+                elif line_as_list[0] in FUNCTION_CALLING_COMMAND:
+                    pass # for project 8
+    return result
+
+
+def get_arithmetic_command_lines(command):
+        if command == "add":
+            return ["//add:",
+                    "@SP",
+                    "A = M - 1",
+                    "D = M",
+                    "@SP",
+                    "M = M - 1",
+                    "A = M - 1",
+                    "M = M + D"]
+        if command == "sub":
+            return ["//sub:",
+                    "@SP",
+                    "A = M - 1",
+                    "D = M",
+                    "@SP",
+                    "M = M - 1",
+                    "A = M - 1",
+                    "M = M - D"]
+        if command == "neg":
+            return ["//neg:",
+                    "@SP",
+                    "A = M - 1",
+                    "M = -M"]
+        if command == "eq":
+            result = ["//" + command + ":",
+                      "@SP",
+                      "A = M - 1",
+                      "D = M // D=y",
+                      "@SP",
+                      "M = M - 1",
+                      "A = M - 1",
+                      "D = M - D // D=x-y"]
+                      #not finished...
+
+
+def get_memory_command_lines(command, segment, index):
+    if command == "push":
+        pass
+    elif command == "pop":
+        pass
+
 # The main program:
 if __name__ == "__main__":
     list_of_files_path = get_files(sys.argv)
+    assembly_lines_list = []
     for file_path in list_of_files_path:
-        assembly_lines_list = [] # some magic here...
+        assembly_lines_list = file_to_assembly_lines(file_path)
         lines_list_to_file(file_output_path(file_path), assembly_lines_list)
